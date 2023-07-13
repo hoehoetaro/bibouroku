c83b467790bd91a35fb3b6b1bcbf658d3611570a
hoehoetaro <hoehoe@kcf.biglobe.ne.jp>
2023-07-14 01:28:38 +0900

    change first element to dropdown list

--- 03_CSV_Generator.py
+++ 03_CSV_Generator.py
@@ -9,12 +9,23 @@ data = []
 fields = []
 
 def create_field(default_command=None):
-    new_field1 = tk.Entry(root)
-    new_field1.insert(0, default_command or '')  # Insert default text if provided
-    new_field1.grid(row=len(fields)+2, column=1)
+    # Define the dropdown options
+    options = ['question', 'answer', 'img', 'video','code','p']  # You can change this list according to your needs
+
+    # Create a StringVar object to hold the selected option
+    selected_option = tk.StringVar(root)
+    if default_command:
+        selected_option.set(default_command)  # Use the default command if provided
+    else:
+        selected_option.set(options[0])  # Use the first option as default
+
+    dropdown = tk.OptionMenu(root, selected_option, *options)
+    dropdown.grid(row=len(fields)+2, column=1)
+    
     new_field2 = tk.Entry(root)
     new_field2.grid(row=len(fields)+2, column=2)
-    fields.append((new_field1, new_field2))
+    
+    fields.append((dropdown, selected_option, new_field2))  # Add the OptionMenu widget itself to the fields list
 
 def add_field():
     create_field()
@@ -22,16 +33,16 @@ def add_field():
 def remove_field():
     if len(fields) > 0:
         field_to_remove = fields.pop()
-        field_to_remove[0].destroy()
-        field_to_remove[1].destroy()
+        field_to_remove[0].grid_forget()  # Hide the dropdown widget
+        field_to_remove[2].destroy()  # Destroy the entry field
 
 def export_to_csv():
     for field in fields:
-        command = field[0].get()  # Now represents command
-        element = field[1].get()  # Now represents element
+        command = field[1].get()  # Now represents command
+        element = field[2].get()  # Now represents element
         if command and element:  # Check if both fields are non-empty
             data.append([command, element])
-    
+
     with open('scenario.csv', 'w', newline='', encoding='utf-8') as f:  # Updated file name
         writer = csv.writer(f)
         writer.writerow(['command', 'element'])  # Add headers
